44403e659e41443216dfe3bd963a9b4fcb37a913
Morgan Blackthorne <stormerider@gmail.com>
2013-05-26 03:05:00 -0700

    Bump version number

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "The Wharton School - The University of Pennsylvania"
 maintainer_email  "bflad@wharton.upenn.edu"
 license           "Apache 2.0"
 description       "Installs, configures, and enables IRC exception handler."
-version           "0.1.1"
+version           "0.1.2"
 recipe            "irc_handler", "Installs, configures, and enables IRC exception handler."
 
 %w{ chef_handler }.each do |cb|
